4c0a6f4b15624a7816f280705e1c1113e70a860a
Joseph Tseng <106340233+Chalkman071@users.noreply.github.com>
2022-12-28 13:16:55 +0800

    updated color in constant.py

--- constant.py
+++ constant.py
@@ -1,6 +1,6 @@
-col_about_to_die = (200, 200, 225) #要死掉的顏色
+col_about_to_die = '#9b6f6f' #要死掉的顏色
 col_alive = (255, 255, 215) #活著的顏色
-col_background = (10, 10, 40) #背景色
+col_background = '#180d26' #背景色
 col_grid = (30, 30, 60) #格線顏色
 col_neighbor = (0, 200, 80) #綠色範圍表示區塊間互相影響
 col_text = '#FFFFFF' #按鈕上字的顏色
