35175fe3ab608674e18254bbb64ab6ffbb10f83c
Joseph Tseng <106340233+Chalkman071@users.noreply.github.com>
2023-01-02 19:57:47 +0800

    modified algorithm_still_v2

--- algorithm_still_v2.py
+++ algorithm_still_v2.py
@@ -1,12 +1,22 @@
 import numpy as np
 from tqdm import tqdm
 from textwrap import wrap
+import pygame
+from update import draw
+from constant import col_grid
 
 with open("still_output_5.txt", "w") as file:
     file.write("Still cells\nInput cell size = 6 * 6\n")
 database = set()
 progress = tqdm(total = 2 ** 36 - 1)
 output = []
+dimx, dimy, cellsize = 82, 50, 10 #顯示用
+pygame.init() #顯示用
+display = pygame.Surface((dimx * cellsize, dimy * cellsize)) #顯示用
+surface = pygame.display.set_mode((dimx * cellsize, dimy * cellsize)) #顯示用
+pygame.display.set_caption("Game of Life") #顯示用
+display_arr = -np.ones((dimy, dimx)) #顯示用
+count = 0 #顯示用
 
 for i in range(1, 2 ** 36):
     progress.update(1)
@@ -19,10 +29,19 @@ for i in range(1, 2 ** 36):
         nstrcell = strcell.strip("0")
         if nstrcell not in database:
             arr = np.array(list(nstrcell + "0" * (36 - len(nstrcell)))).reshape((6, 6))
-            output.append(arr)
-            draw(output)
+            output.append(np.array(list(strcell + "0" * (36 - len(strcell)))).reshape((6, 6)))
+            column, row = divmod(count, 6) #顯示用
+            column %= 10 #顯示用
+            display_arr[2+8*row:8+8*row, 2+8*column:8+8*column] = arr #顯示用
+            surface.fill(col_grid) #顯示用
+            draw(surface, display_arr, dimx, dimy, cellsize, True) #顯示用
+            pygame.display.update() #顯示用
+            count += 1 #顯示用
             for array in (arr, arr.T, arr[::-1], arr[::-1].T, arr.T[::-1], arr.T[::-1].T, arr[::-1].T[::-1], arr[::-1].T[::-1].T):
                 database.add("".join(array.astype(str).flatten()).strip("0").rstrip("0"))
             # with open("still_output_5.txt", "a") as file:
             #     file.write("\n".join(i for i in wrap(strcell, 6) if i != "000000" and len(i) == 6) + "\n\n")
+    for event in pygame.event.get(): #顯示用
+        if event.type == pygame.QUIT: #顯示用
+            pygame.quit() #顯示用
 
--- update.py
+++ update.py
@@ -13,15 +13,23 @@ def update(surface, cells, dimx, dimy, cellsize, running): # cells: current stat
                 nxt[r, c] = 1
     return nxt
 
-def draw(surface, nxt, dimx, dimy, cellsize):
+def draw(surface, nxt, dimx, dimy, cellsize, all_alive=False):
     for r, c in np.ndindex(nxt.shape):
         num_alive = np.sum(nxt[r-1:r+2, c-1:c+2]) - nxt[r, c] # 周圍還活著的
         col = constant.col_background
         # if num_alive > 0:
         #     col = constant.col_neighbor
-        if (nxt[r, c] == 1 and constant.min_num_alive <= num_alive <= constant.max_num_alive):
-            col = constant.col_alive
-        elif nxt[r, c] == 1 and (num_alive < constant.min_num_alive or num_alive > constant.max_num_alive):
-            col = constant.col_about_to_die
+        if all_alive:
+            if nxt[r, c] == 1:
+                col = constant.col_alive
+            elif nxt[r, c] == 0:
+                col = constant.col_background
+            else:
+                col = constant.col_grid
+        else: 
+            if (nxt[r, c] == 1 and constant.min_num_alive <= num_alive <= constant.max_num_alive):
+                col = constant.col_alive
+            elif nxt[r, c] == 1 and (num_alive < constant.min_num_alive or num_alive > constant.max_num_alive):
+                col = constant.col_about_to_die
         # col = col if cells[r, c] == 1 or col == col_neighbor else col_background
         pygame.draw.rect(surface, col, (c*cellsize, r*cellsize, cellsize-1, cellsize-1)) #更新方格顏色
